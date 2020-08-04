1666d3c3c1c1076a67183759d65bbb6f97950a63
stuartlogan82 <slogan@twilio.com>
2020-08-04 13:09:10 +0100

    added support for https endpoint

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://localhost:5000',
         ws: false,
         changeOrigin: true,
       },
